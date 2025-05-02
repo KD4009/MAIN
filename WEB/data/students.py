6857368ee306f413c0835c2b126a51bcc59fc963
KD4009 <kvondeniz@yandex.ru>
2025-05-02 21:29:05 +0700

    Merge remote-tracking branch 'origin/main'

---
+++ WEB/data/students.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from models import db_session
+from models.db_session import SqlAlchemyBase
+
+
+class Students(SqlAlchemyBase):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    place = Column(String, nullable=False)
+    author = Column(Integer, nullable=False)  # ID пользователя, добавившего участника
